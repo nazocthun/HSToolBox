125b2086b808f7a305aa74e28a38c6998119b584
nazocthun <nazocthun@gmail.com>
2023-02-03 09:56:35 +0800

    fix: unused action

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,14 +6,10 @@ export const useToolBoxStore = defineStore('toolbox', () => {
   const selectedMode = ref('index')
   const selectedMenu = ref<string>('index')
 
-  function changeSelectedModeTo(newMode: string) {
-    selectedMode.value = newMode
-  }
   return {
     clientWidth,
     showMenu,
     selectedMode,
     selectedMenu,
-    changeSelectedModeTo,
   }
 })
