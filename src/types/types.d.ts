7938170b1ddd45ea37c3c00ff0592e5bf5ffba8d
nazocthun <nazocthun@gmail.com>
2023-03-12 21:13:51 +0800

    chore: type update

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,5 +1,32 @@
 declare interface Card { // 卡牌
   card_name: string,
   card_id: string,
+  golden_id?: string,
+  golden_card: Card,
   version?: string,
+  associated_ids?: string[],
+  associated_cards: Card[],
+}
+
+declare interface TavernBuddy { // 卡牌
+  buddy_id: string,
+  buddy_name: string,
+  buddy_info: string,
+}
+
+declare interface Hero {
+  hero_id: string,
+  hero_name: string,
+  armor_level: number,
+  race_bound: string,
+  skill: TavernHeroSkill,
+  buddy: TavernBuddy,
+}
+
+declare interface TavernHeroSkill {
+  skill_id: string,
+  skill_name: string,
+  skill_cost: string,
+  skill_info: string,
+  associated?: TavernHeroSkill[],
 }
