6989a7dce8fe2f42965f8c2032086fecc43ad39d
nazocthun <nazocthun@gmail.com>
2023-03-21 11:29:27 +0800

    feat: divider with a label

---
+++ src/components/LabelDivider.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{ label: string }>()
+</script>
+
+<template>
+  <div flex>
+    <div flex-none pr-3 text-xl flex items-center>
+      {{ props.label }}
+    </div>
+    <n-divider />
+  </div>
+</template>
