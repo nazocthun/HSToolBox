60fb586b5e992ad4feb773bdf7c0400cc41c98db
nazocthun <nazocthun@gmail.com>
2023-03-21 11:28:55 +0800

    feat: more pages to write

---
+++ src/pages/BattleGrounds/Buddies.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    Battlegrounds Buddies
+  </div>
+</template>
+
+<route>
+{
+  name: 'BattleGrounds-Buddies'
+}
+</route>
---
+++ src/pages/BattleGrounds/Quests.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    Battlegrounds Quests
+  </div>
+</template>
+
+<route>
+{
+  name: 'BattleGrounds-Quests'
+}
+</route>
